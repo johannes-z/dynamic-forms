b2f8e774cddf653f1c87c8c9b783081e7271a70a
Johannes Zwirchmayr <johannes.zwirchmayr@gmail.com>
2018-01-21 15:34:07 +0100

    first commit

---
+++ src/components/FormElement.vue
@@ -0,0 +1,49 @@
+<docs>
+  # TODO
+
+  value binding
+</docs>
+
+<template>
+  <component :is="tag" v-bind="$attrs">
+    <slot>
+      <FormElement
+        v-for="(formElement, index) in children"
+        :key="index"
+        :tag="formElement.tag"
+        :children="formElement.children"
+      ></FormElement>
+    </slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'FormElement',
+  props: {
+    tag: {
+      type: String,
+      required: true
+    },
+    children: {
+      type: Array,
+      required: false,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    addformElement (formElement) {
+      this.formElements.children.push(formElement)
+    },
+    removeformElement (index) {
+      this.formElements.children.splice(index, 1)
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <!-- <FormInput tag="input" type="number" /> -->
+
+    <FormElement tag="div">
+      <FormElement tag="input" type="number" />
+    </FormElement>
+
+    <!-- <FormInput v-for="(formInput, index) in formElements" :key="index"
+      :tag="formInput.tag"
+      :children="formInput.children">
+    </FormInput> -->
+  </div>
+</template>
+
+<script>
+import FormElement from './FormElement.vue'
+
+export default {
+  components: {
+    FormElement
+  },
+  data () {
+    return {
+      formElements: [
+        {
+          tag: 'div',
+          children: [
+            { tag: 'input' }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
